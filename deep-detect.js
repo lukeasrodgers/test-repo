3d323cca1bc7d0eccc0f5d93776c26e4654e7016
Luke Rodgers <lukeasrodgers@gmail.com>
2013-07-30 17:36:12 -0400

    Adding some random code

---
+++ deep-detect.js
@@ -0,0 +1,25 @@
+var deepDetect = function(list, iterator, context) {
+  var _deepDetect = function(list, iterator, context) {
+    if (_.isArray(list[0])) {
+      _.each(list, function(sublist) {
+        return _deepDetect(sublist, iterator, context); 
+      });
+    }
+    else {
+      var res = _.detect(list, iterator, context);
+      if (!_.isUndefined(res)) {
+        throw res;
+      }
+    }
+  };
+  try {
+    _deepDetect(list, iterator, context);
+  }
+  catch (err) {
+    return err;
+  }
+};
+ 
+_.mixin({
+  deepDetect: deepDetect
+});
